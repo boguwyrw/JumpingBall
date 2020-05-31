5fd201c1227cd5f1cb5f14bf6dbd0860566be4cd
boguwyrw <b.wyrwal@wp.pl>
2020-05-31 22:13:12 +0200

    Creating map - in progress
    Tasks:
    1. Third (and last) stage of level
    2. System control for smartphone
    3. Control player by buttons

---
+++ JumpingBall/Assets/Scripts/JumpingEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingEnemy : MonoBehaviour
+{
+
+    private Rigidbody jumpingEnemyRigidbody;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        jumpingEnemyRigidbody = GetComponent<Rigidbody>();
+        jumpingEnemyRigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        isGrounded = true;
+    }
+
+    private void Update()
+    {
+        if (isGrounded)
+        {
+            jumpingEnemyRigidbody.AddForce(Vector3.up * 2.75f, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+}
---
+++ JumpingBall/Assets/Scripts/ManuSceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManuSceneController : MonoBehaviour
+{
+
+    public Button playButton;
+    public Button optionsButton;
+    public Button exitButton;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
